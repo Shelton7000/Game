96a798d7528364b6eb66af62ecdcccf6e6001cd0
Shelton7000 <hoagie7000@gmail.com>
2016-12-09 01:19:52 -0500

    Update Roach.java

--- src/Roach.java
+++ src/Roach.java
@@ -1,29 +1,37 @@
 
+
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Roach {
 
-	 
-	public BufferedImage getImage(String pic) {
-		BufferedImage image=null;
+	BufferedImage image=null;
+	public BufferedImage getImage(String pic) 
+	{
 		try 
 		{
 	          image = ImageIO.read(getClass().getResource(pic)) ;
 
+              
+       } 
+		catch (IOException e) 
+		{
+         e.printStackTrace();
+        }
 
-     } catch (IOException e) {
-e.printStackTrace();
-}
-
-	return image;
+	   return image;
 	}
+ public void closeImage()
+ {
+	 image.flush();
+ }
 
-
-public static void main(String[] args) {
-Roach ri = new Roach();
-String string=null;
-ri.getImage(string);
-}
+   public static void main(String[] args) 
+   {
+    Roach ri = new Roach();
+    String string=null; 
+   
+    ri.getImage(string);
+   }
 }
